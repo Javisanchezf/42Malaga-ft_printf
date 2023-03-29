33e9e6160eaeba8e86533c336eb9c56504099fd6
javiersa <javiersa@student.42malaga.com>
2023-03-29 19:23:11 +0200

    AUTOMATIC COMMIT

--- ft_printf_hex.c
+++ ft_printf_hex.c
@@ -6,7 +6,7 @@
 /*   By: javiersa <javiersa@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:27:12 by javiersa          #+#    #+#             */
-/*   Updated: 2022/12/19 16:30:18 by javiersa         ###   ########.fr       */
+/*   Updated: 2023/03/29 19:22:33 by javiersa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,30 @@ static void	ft_showhex(unsigned long nbr, char *base)
 		ft_printfchar(base[nbr]);
 }
 
+static void	ft_showhexint(unsigned int nbr, char *base)
+{
+	if (nbr >= 16)
+	{
+		ft_showhex(nbr / 16, base);
+		ft_showhex(nbr % 16, base);
+	}
+	else
+		ft_printfchar(base[nbr]);
+}
+
 static	int	ft_counthex(unsigned long v)
+{
+	int	i;
+
+	i = 0;
+	if (v == 0)
+		i++;
+	while (v >= 1 && i++ >= 0)
+		v = v / 16;
+	return (i);
+}
+
+static	int	ft_counthexint(unsigned long v)
 {
 	int	i;
 
@@ -48,8 +71,8 @@ int	ft_printfhex(void *v, char type)
 		return (ft_counthex((unsigned long)v) + i);
 	}
 	if (type == 'x')
-		ft_showhex((unsigned int)v, "0123456789abcdef");
+		ft_showhexint((unsigned int)v, "0123456789abcdef");
 	else if (type == 'X')
-		ft_showhex((unsigned int)v, "0123456789ABCDEF");
-	return (ft_counthex((unsigned int)v) + i);
+		ft_showhexint((unsigned int)v, "0123456789ABCDEF");
+	return (ft_counthexint((unsigned int)v) + i);
 }
